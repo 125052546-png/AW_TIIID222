e83cee524e12a8d4e8f92666140a593602bd2c44
125052546-png <125052546@upq.edu.mx>
2026-09-17 17:18:38 -0600

    Trabajamos con php y estructura basica de html

---
+++ index.php
@@ -0,0 +1,35 @@
+<!-- <?php
+echo "ola";
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1 {
+            color: blue;
+        }
+        div {
+            background: blue;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hola crayola</h1>
+    <h2>Hola crayola</h2>
+    <h3>Hola crayola</h3>
+    <h4>Hola crayola</h4>
+    <h5>Hola crayola</h5>
+    <h6>Hola crayola</h6>
+
+    <div>
+        
+        <p>Este es un parrafo</p>
+    </div>
+
+
+</body>
+</html>
